458e4c0097c8e43f9b2b6cf12704d419481f49f8
pavansai018 <18pavansai@gmail.com>
2025-11-24 21:50:53 +0800

    Improved UX

--- nodes/open_loop_control.py
+++ nodes/open_loop_control.py
@@ -20,9 +20,9 @@ class TeleopButtons(Node):
     def create_gui(self):
         """Create the button GUI"""
         self.root = tk.Tk()
-        self.root.title("Robot Teleoperation")
-        self.root.geometry("300x400")
-        self.root.resizable(False, False)
+        self.root.title("Dummy Robot Teleoperation")
+        self.root.geometry("500x500")
+        self.root.resizable(True, True)
         
         # Configure style
         style = ttk.Style()
@@ -35,7 +35,7 @@ class TeleopButtons(Node):
             text="FORWARD", 
             command=self.move_forward
         )
-        self.forward_btn.place(relx=0.5, rely=0.2, anchor='center', width=100, height=60)
+        self.forward_btn.place(relx=0.5, rely=0.2, anchor='center', width=150, height=60)
         
         # Stop button (center)
         self.stop_btn = ttk.Button(
@@ -45,7 +45,7 @@ class TeleopButtons(Node):
             style='Emergency.TButton'
         )
         style.configure('Emergency.TButton', background='red', foreground='white')
-        self.stop_btn.place(relx=0.5, rely=0.5, anchor='center', width=100, height=60)
+        self.stop_btn.place(relx=0.5, rely=0.5, anchor='center', width=150, height=60)
         
         # Backward button (bottom)
         self.backward_btn = ttk.Button(
@@ -53,7 +53,7 @@ class TeleopButtons(Node):
             text="BACKWARD", 
             command=self.move_backward
         )
-        self.backward_btn.place(relx=0.5, rely=0.8, anchor='center', width=100, height=60)
+        self.backward_btn.place(relx=0.5, rely=0.8, anchor='center', width=150, height=60)
         
         # Left button (left of stop)
         self.left_btn = ttk.Button(
@@ -61,7 +61,7 @@ class TeleopButtons(Node):
             text="LEFT", 
             command=self.turn_left
         )
-        self.left_btn.place(relx=0.2, rely=0.5, anchor='center', width=80, height=60)
+        self.left_btn.place(relx=0.2, rely=0.5, anchor='center', width=100, height=60)
         
         # Right button (right of stop)
         self.right_btn = ttk.Button(
@@ -69,7 +69,7 @@ class TeleopButtons(Node):
             text="RIGHT", 
             command=self.turn_right
         )
-        self.right_btn.place(relx=0.8, rely=0.5, anchor='center', width=80, height=60)
+        self.right_btn.place(relx=0.8, rely=0.5, anchor='center', width=100, height=60)
         
         # Status label
         self.status_label = ttk.Label(
@@ -90,14 +90,14 @@ class TeleopButtons(Node):
 
     def move_forward(self):
         """Move robot forward"""
-        self.current_cmd.linear.x = 0.5
+        self.current_cmd.linear.x = 1.0
         self.current_cmd.angular.z = 0.0
         self.publish_cmd()
         self.update_status("Moving FORWARD")
 
     def move_backward(self):
         """Move robot backward"""
-        self.current_cmd.linear.x = -0.3
+        self.current_cmd.linear.x = -1.0
         self.current_cmd.angular.z = 0.0
         self.publish_cmd()
         self.update_status("Moving BACKWARD")
@@ -105,14 +105,14 @@ class TeleopButtons(Node):
     def turn_left(self):
         """Turn robot left (in place)"""
         self.current_cmd.linear.x = 0.0
-        self.current_cmd.angular.z = 0.5
+        self.current_cmd.angular.z = 1.0
         self.publish_cmd()
         self.update_status("Turning LEFT")
 
     def turn_right(self):
         """Turn robot right (in place)"""
         self.current_cmd.linear.x = 0.0
-        self.current_cmd.angular.z = -0.5
+        self.current_cmd.angular.z = -1.0
         self.publish_cmd()
         self.update_status("Turning RIGHT")
 
