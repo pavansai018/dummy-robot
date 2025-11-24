6de907e7646f3acc075b736474221f9bcc1c534d
pavansai018 <18pavansai@gmail.com>
2025-11-24 14:53:34 +0800

    Added launch, urdf

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'dummy_robot'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.*'))),
+        (os.path.join('share', package_name, 'urdf/accessories'), glob(os.path.join('urdf', 'accessories', '*.*'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
